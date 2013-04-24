3be081cf40a665a5167502f87a8a6769a06d1e73
Jeroen Hoebe <hoebejeroen@gmail.com>
2013-04-24 18:40:43 +0200

    change sockets

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
 
-  socket.on('receive_clients', function(data){
+  socket.on('clients', function(data){
     console.log(data);
-    socket.emit('send_spotify', {songId: 'song id send from client'});
+    socket.emit('tospotify', {songId: 'sendToSpotify'});
   });
 });
