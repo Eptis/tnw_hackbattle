e6712bdbeda5ae00f90d549f2e4e9c685b6f57f0
Jeroen Hoebe <hoebejeroen@gmail.com>
2013-04-25 10:30:51 +0200

    setting up server

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var io = require('socket.io').listen(8080);
+console.log("socket io running on port 8080");
 
 io.sockets.on('connection', function (socket) {
-  // socket.emit('news', { hello: 'world' });
-  // socket.on('my other event', function (data) {
-  //   console.log(data);
-  // });
-
   socket.on('clients', function(data){
     console.log(data);
     
     var receivedSong = data.sendSongId;
     socket.broadcast.emit('tospotify', {songId: receivedSong});
   });
+});
 
-  
+
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+    console.log("http server running on port 8000");
 });
