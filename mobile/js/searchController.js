97a80fa0f604d7b3f379ac6646300991ec73ac0b
Dennis Tel <dennistel90@gmail.com>
2013-04-25 12:43:41 +0200

    updated

--- mobile/js/searchController.js
+++ mobile/js/searchController.js
@@ -1,8 +1,13 @@
-app.controller('SearchController', function($scope, $http) {
+app.controller('SearchController', function($scope, $http, $timeout) {
     $scope.search_input = '';
     $scope.added_songs = [];
+        var count=0;
+
+
     $scope.getData = function (query){
-        var url = "http://ws.spotify.com/search/1/track.json?q=" + query
+        // var url = "http://ws.spotify.com/search/1/track.json?q=" + query
+        var url = "http://rijksmuseum.donnywals.com/feed.json"
+
         $http.get(url).then(function(res){
             if(res.data.tracks.length == 0){
                 $scope.no_results = true;
@@ -12,10 +17,32 @@ app.controller('SearchController', function($scope, $http) {
             $scope.data = res.data.tracks;                
         });
     }
-    $scope.sendSong = function(songId){
+    $scope.sendSong = function(songId, songName){
         var socket = io.connect('http://87.255.55.193:8080/');
+        $scope.showNotif = true;
         socket.emit('clients', {sendSongId: songId});  
-        $scope.added_songs.push(songId)
+        $scope.added_songs.push(songId);
+        
+        $scope.songName = songName;
+        
+        if(count == 0){
+           var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+        }
+        count = 3;
+        function timer()
+        {
+          count = count-1;
+          if (count <= 0)
+          {
+            $scope.$apply(function(){
+                    $scope.showNotif = false;
+                }
+            );
+             clearInterval(counter);
+             return;
+          }
+        }
+
     }
 
     $scope.checkIfAdded = function(songId){
