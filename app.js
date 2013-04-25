2e4ede7330c116469b9f3c18e9eceedb9def340b
Jeroen Hoebe <hoebejeroen@gmail.com>
2013-04-25 10:55:13 +0200

    serve js the right way

--- app.js
+++ app.js
@@ -47,14 +47,3 @@ http.createServer(function(request, response) {
 	});
 })
 
-fs.readFile('./mobile/index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-    console.log("http server running on port 8000");
-});
